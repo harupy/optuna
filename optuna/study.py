c781b30b80c4e0b5d6df64fe4d5cbf425652f19a
harupy <17039389+harupy@users.noreply.github.com>
2020-09-08 15:10:33 +0900

    commit

--- optuna/study.py
+++ optuna/study.py
@@ -675,7 +675,10 @@ class Study(BaseStudy):
                 callback(self, frozen_trial)
 
     def _run_trial(
-        self, func: ObjectiveFuncType, catch: Tuple[Type[Exception], ...], gc_after_trial: bool,
+        self,
+        func: ObjectiveFuncType,
+        catch: Tuple[Type[Exception], ...],
+        gc_after_trial: bool,
     ) -> trial_module.Trial:
 
         # Sync storage once at the beginning of the objective evaluation.
@@ -923,7 +926,10 @@ def load_study(
     return Study(study_name=study_name, storage=storage, sampler=sampler, pruner=pruner)
 
 
-def delete_study(study_name: str, storage: Union[str, storages.BaseStorage],) -> None:
+def delete_study(
+    study_name: str,
+    storage: Union[str, storages.BaseStorage],
+) -> None:
     """Delete a :class:`~optuna.study.Study` object.
 
     Args:
@@ -948,7 +954,25 @@ def get_all_study_summaries(storage: Union[str, storages.BaseStorage]) -> List[S
 
     Example:
 
+        .. testsetup::
 
+            import os
+
+            if os.path.exists("example.db"):
+                raise RuntimeError("'example.db' already exists. Please remove it.")
+
+        .. testcode::
+
+            import optuna
+
+            def objective(trial):
+                x = trial.suggest_float("x", -10, 10)
+                return (x - 2) ** 2
+
+
+        .. testcleanup::
+
+            os.remove("example.db")
 
     Args:
         storage:
